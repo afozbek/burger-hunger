71e4e89475b7df7969b530c1804e543b20fcd25a
afozbek <furkanozbek1995@gmail.com>
2019-03-31 20:36:18 +0300

    calling BurgerBuilder

--- src/App.js
+++ src/App.js
@@ -1,14 +1,14 @@
 import React, { Component } from 'react';
 
 import Layout from './components/Layout/Layout';
-
+import BurgerBuilder from './containers/BurgerBuilder/BurgerBuilder';
 class App extends Component {
   render() {
     return (
       <div>
         <Layout>
-          <p>Testing</p>
           {/* Some Component will come here */}
+          <BurgerBuilder />
         </Layout>
 
       </div>
