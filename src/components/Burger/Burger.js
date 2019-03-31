0b423d3dd0f307c6bee52e25cc1de8be4759d77d
afozbek <furkanozbek1995@gmail.com>
2019-03-31 21:38:20 +0300

    managing ingredients via array methods

--- src/components/Burger/Burger.js
+++ src/components/Burger/Burger.js
@@ -4,11 +4,19 @@ import PropTypes from "prop-types";
 import classes from "./Burger.css";
 import BurgerIngredient from "./BurgerIngredient/BurgerIngredient";
 const burger = props => {
-  const transformedIngredients = Object.keys(props.ingredients).map(ingKey => {
-    return [...Array(props.ingredients[ingKey])].map((_, i) => {
-      return <BurgerIngredient key={ingKey + i} type={ingKey} />;
-    });
-  });
+  let transformedIngredients = Object.keys(props.ingredients)
+    .map(ingKey => {
+      return [...Array(props.ingredients[ingKey])].map((_, i) => {
+        return <BurgerIngredient key={ingKey + i} type={ingKey} />;
+      });
+    })
+    .reduce((arr, el) => {
+      return arr.concat(el);
+    }, []);
+
+  if (transformedIngredients.length === 0) {
+    transformedIngredients = <p>Please start adding ingredients</p>;
+  }
   return (
     <div className={classes.Burger}>
       <BurgerIngredient type="bread-top" />
