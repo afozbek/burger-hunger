3bdcc52271e70df07bd2ee08215ae477d8c216fc
afozbek <furkanozbek1995@gmail.com>
2019-03-31 20:38:18 +0300

    calling Burger component

--- src/containers/BurgerBuilder/BurgerBuilder.js
+++ src/containers/BurgerBuilder/BurgerBuilder.js
@@ -1,18 +1,19 @@
-import React, { Component } from 'react';
-import Auxiliary from '../../hoc/Auxiliary';
+import React, { Component } from "react";
+import Auxiliary from "../../hoc/Auxiliary";
 
+import Burger from "../../components/Burger/Burger";
 class BurgerBuilder extends Component {
-    constructor(props) {
-        super(props);
-    }
-    render() {
-        return (
-            <Auxiliary>
-                <div>Burger</div>
-                <div>Build Controls</div>
-            </Auxiliary>
-        );
-    }
+  constructor(props) {
+    super(props);
+  }
+  render() {
+    return (
+      <Auxiliary>
+        <Burger />
+        <div>Build Controls</div>
+      </Auxiliary>
+    );
+  }
 }
 
 export default BurgerBuilder;
