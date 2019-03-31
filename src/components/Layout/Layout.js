6d56eb28a1faab0edb62dcad1865be36994e2741
afozbek <furkanozbek1995@gmail.com>
2019-03-31 19:42:20 +0300

    adding demo classes from css module

--- src/components/Layout/Layout.js
+++ src/components/Layout/Layout.js
@@ -1,12 +1,14 @@
 import React from 'react'
 import Auxiliary from '../../hoc/Auxiliary';
+import classes from './Layout.css';
+
 const layout = (props) => (
     <Auxiliary>
         <div>
             {/* Some Style will come here */}
             Toolbar, SideDrawer, Backdrop
         </div>
-        <main>
+        <main className={classes.Content}>
             {props.children}
         </main>
     </Auxiliary>
