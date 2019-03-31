a2eb6bd3a9a5a30a15bb0ed528a6c153dc0593a0
afozbek <furkanozbek1995@gmail.com>
2019-03-31 21:04:53 +0300

    transformin the ingredient object

--- src/components/Burger/Burger.js
+++ src/components/Burger/Burger.js
@@ -4,11 +4,15 @@ import PropTypes from "prop-types";
 import classes from "./Burger.css";
 import BurgerIngredient from "./BurgerIngredient/BurgerIngredient";
 const burger = props => {
+  const transformedIngredients = Object.keys(props.ingredients).map(ingKey => {
+    return [...Array(props.ingredients[ingKey])].map((_, i) => {
+      return <BurgerIngredient key={ingKey + i} type={ingKey} />;
+    });
+  });
   return (
     <div className={classes.Burger}>
       <BurgerIngredient type="bread-top" />
-      <BurgerIngredient type="cheese" />
-      <BurgerIngredient type="meat" />
+      {transformedIngredients}
       <BurgerIngredient type="bread-bottom" />
     </div>
   );
