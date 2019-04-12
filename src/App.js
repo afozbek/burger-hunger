93d1c572682e971c0fd627e616feca056b6ac16a
afozbek <furkanozbek1995@gmail.com>
2019-04-12 18:27:27 +0300

    kaldigim yer 😊

--- src/App.js
+++ src/App.js
@@ -1,16 +1,16 @@
-import React, { Component } from 'react';
+import React, { Component } from "react";
 
-import Layout from './components/Layout/Layout';
-import BurgerBuilder from './containers/BurgerBuilder/BurgerBuilder';
+import Layout from "./components/Layout/Layout";
+import BurgerBuilder from "./containers/BurgerBuilder/BurgerBuilder";
 class App extends Component {
   render() {
     return (
       <div>
+        <h1>Bolum 8 --> 135.videodayım</h1>
         <Layout>
           {/* Some Component will come here */}
           <BurgerBuilder />
         </Layout>
-
       </div>
     );
   }
