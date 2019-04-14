92d22e3b0315a7339c122e5db8fff220c6215507
afozbek <furkanozbek1995@gmail.com>
2019-04-14 20:09:21 +0300

    updating layout

--- src/components/Layout/Layout.js
+++ src/components/Layout/Layout.js
@@ -1,13 +1,31 @@
-import React from "react";
+import React, { Component } from "react";
 import Auxiliary from "../../hoc/Auxiliary";
 import classes from "./Layout.css";
 import Toolbar from "../Navigation/Toolbar/Toolbar";
 import SideDrawer from "../Navigation/SideDrawer/SideDrawer";
-const layout = props => (
-  <Auxiliary>
-    <Toolbar />
-    <SideDrawer />
-    <main className={classes.Content}>{props.children}</main>
-  </Auxiliary>
-);
-export default layout;
+
+class Layout extends Component {
+  state = {
+    showSideDrawer: true
+  };
+
+  sideDrawerClosedHandler = () => {
+    this.setState({
+      showSideDrawer: false
+    });
+  };
+
+  render() {
+    return (
+      <Auxiliary>
+        <Toolbar />
+        <SideDrawer
+          open={this.state.showSideDrawer}
+          closed={this.sideDrawerClosedHandler}
+        />
+        <main className={classes.Content}>{this.props.children}</main>
+      </Auxiliary>
+    );
+  }
+}
+export default Layout;
