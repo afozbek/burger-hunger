0c00988db866129538879ff8b0d3419bbca4cae1
afozbek <furkanozbek1995@gmail.com>
2019-04-14 20:21:30 +0300

    adding handler to layout

--- src/components/Layout/Layout.js
+++ src/components/Layout/Layout.js
@@ -14,11 +14,16 @@ class Layout extends Component {
       showSideDrawer: false
     });
   };
+  sideDrawerToggleHandler = () => {
+    this.setState(prevState => {
+      return { showSideDrawer: !prevState.showSideDrawer };
+    });
+  };
 
   render() {
     return (
       <Auxiliary>
-        <Toolbar />
+        <Toolbar drawerToggleClick={this.sideDrawerToggleHandler} />
         <SideDrawer
           open={this.state.showSideDrawer}
           closed={this.sideDrawerClosedHandler}
