659af3747480c02ab7bc159e00db6277f5e4af54
afozbek <furkanozbek1995@gmail.com>
2019-04-14 18:26:06 +0300

    implementing Logo to Toolbar

--- src/components/Navigation/Toolbar/Toolbar.js
+++ src/components/Navigation/Toolbar/Toolbar.js
@@ -1,9 +1,11 @@
 import React from "react";
 import classses from "./Toolbar.css";
+
+import Logo from "../../Logo/Logo";
 const toolbar = props => (
   <header className={classses.Toolbar}>
     <div>MENU</div>
-    <div>LOGO</div>
+    <Logo />
     <nav>...</nav>
   </header>
 );
