ca48f3702e7b722937ead798dee84d1372216572
afozbek <furkanozbek1995@gmail.com>
2019-04-19 19:27:28 +0300

    using Route to make routes

--- src/App.js
+++ src/App.js
@@ -2,13 +2,19 @@ import React, { Component } from "react";
 
 import Layout from "./hoc/Layout/Layout";
 import BurgerBuilder from "./containers/BurgerBuilder/BurgerBuilder";
+import Checkout from "./containers/Checkout/Checkout";
+import { Route, Link, Switch } from "react-router-dom";
+
 class App extends Component {
   render() {
     return (
       <div>
         <Layout>
           {/* Some Component will come here */}
-          <BurgerBuilder />
+          <Switch>
+            <Route path="/checkout" component={Checkout} />
+            <Route path="/" component={BurgerBuilder} />
+          </Switch>
         </Layout>
       </div>
     );
