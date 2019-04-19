91995135feba9a28428b46ec1947f93c45409451
afozbek <furkanozbek1995@gmail.com>
2019-04-19 20:49:59 +0300

    adding lifecycle to Checkout.js

--- src/containers/Checkout/Checkout.js
+++ src/containers/Checkout/Checkout.js
@@ -11,10 +11,31 @@ export default class Checkout extends Component {
       bacon: 2
     }
   };
+  componentDidMount() {
+    const query = new URLSearchParams(this.props.location.search);
+    const ingredients = {};
+    for (let param of query.entries()) {
+      // param = ['salad', '1'] {[],[],[]}
+      ingredients[param[0]] = +param[1];
+    }
+    this.setState({
+      ingredients: ingredients
+    });
+  }
+  checkoutCancelledHandler = () => {
+    this.props.history.goBack();
+  };
+  onCheckoutContinuedHandler = () => {
+    this.props.history.replace("/checkout/contact-data");
+  };
   render() {
     return (
       <div>
-        <CheckoutSummary ingredients={this.state.ingredients} />
+        <CheckoutSummary
+          ingredients={this.state.ingredients}
+          checkoutCancelled={this.checkoutCancelledHandler}
+          checkoutContinued={this.onCheckoutContinuedHandler}
+        />
       </div>
     );
   }
