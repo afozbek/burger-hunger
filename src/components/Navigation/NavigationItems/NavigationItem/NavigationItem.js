025b4422a0ef70824e958f68a168030e6cfd796e
afozbek <furkanozbek1995@gmail.com>
2019-04-19 22:11:23 +0300

    updating Navs

--- src/components/Navigation/NavigationItems/NavigationItem/NavigationItem.js
+++ src/components/Navigation/NavigationItems/NavigationItem/NavigationItem.js
@@ -1,11 +1,16 @@
 import React from "react";
 import classes from "./NavigationItem.css";
+import { NavLink } from "react-router-dom";
 
 const NavigationItem = props => (
   <li className={classes.NavigationItem}>
-    <a href={props.link} className={props.active ? classes.active : null}>
+    <NavLink
+      activeClassName={classes.active}
+      to={props.link}
+      exact={props.exact}
+    >
       {props.children}
-    </a>
+    </NavLink>
   </li>
 );
 
--- src/components/Navigation/NavigationItems/NavigationItems.js
+++ src/components/Navigation/NavigationItems/NavigationItems.js
@@ -4,10 +4,10 @@ import NavigationItem from "./NavigationItem/NavigationItem";
 
 const navigationItems = props => (
   <ul className={classes.NavigationItems}>
-    <NavigationItem link="#" active>
+    <NavigationItem link="/" exact>
       Burger Builder
     </NavigationItem>
-    <NavigationItem link="#">Checkout</NavigationItem>
+    <NavigationItem link="/orders">Orders</NavigationItem>
   </ul>
 );
 
