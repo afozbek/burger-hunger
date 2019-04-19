95879ba26fe7d2d484fe59300932a775abf5d740
afozbek <furkanozbek1995@gmail.com>
2019-04-19 22:56:58 +0300

    adding lifecycle logic to Orders.js

--- src/containers/Orders/Orders.js
+++ src/containers/Orders/Orders.js
@@ -1,15 +1,50 @@
 import React, { Component } from "react";
-
+import axios from "../../axios-orders";
+import withErrorHandler from "../../hoc/withErrorHandler/withErrorHandler";
 import Order from "../../components/Order/Order";
 
 class Orders extends Component {
+  state = {
+    orders: [],
+    loading: true
+  };
+  componentDidMount() {
+    console.log("adad");
+
+    axios
+      .get("/orders.json")
+      .then(res => {
+        console.log(res.data);
+
+        const fetchedOrders = [];
+        for (let key in res.data) {
+          fetchedOrders.push({
+            ...res.data[key],
+            id: key
+          });
+        }
+        console.log(fetchedOrders);
+
+        this.setState({ loading: false, orders: fetchedOrders });
+      })
+      .catch(err => {
+        this.setState({ loading: false });
+      });
+  }
   render() {
     return (
       <div>
-        <Order />
-        <Order />
+        {this.state.orders.map(order => {
+          return (
+            <Order
+              key={order.id}
+              ingredients={order.ingredients}
+              price={+order.price}
+            />
+          );
+        })}
       </div>
     );
   }
 }
-export default Orders;
+export default withErrorHandler(Orders, axios);
