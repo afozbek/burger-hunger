3883d50129dca12da40044ff3c48e53b005504b1
afozbek <furkanozbek1995@gmail.com>
2019-04-19 21:59:54 +0300

    adding route to orders

--- src/App.js
+++ src/App.js
@@ -4,6 +4,7 @@ import Layout from "./hoc/Layout/Layout";
 import BurgerBuilder from "./containers/BurgerBuilder/BurgerBuilder";
 import Checkout from "./containers/Checkout/Checkout";
 import { Route, Link, Switch } from "react-router-dom";
+import Orders from "./containers/Orders/Orders";
 
 class App extends Component {
   render() {
@@ -13,6 +14,7 @@ class App extends Component {
           {/* Some Component will come here */}
           <Switch>
             <Route path="/checkout" component={Checkout} />
+            <Route path="/orders" component={Orders} />
             <Route path="/" component={BurgerBuilder} />
           </Switch>
         </Layout>
