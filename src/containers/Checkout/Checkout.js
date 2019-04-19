1805b24d247cc12c89cf42273e98817f87f045be
afozbek <furkanozbek1995@gmail.com>
2019-04-19 19:28:08 +0300

    creating Checkout container

---
+++ src/containers/Checkout/Checkout.js
@@ -0,0 +1,21 @@
+import React, { Component } from "react";
+
+import CheckoutSummary from "../../components/Order/CheckoutSummary/CheckoutSummary";
+
+export default class Checkout extends Component {
+  state = {
+    ingredients: {
+      salad: 1,
+      meat: 1,
+      cheese: 1,
+      bacon: 2
+    }
+  };
+  render() {
+    return (
+      <div>
+        <CheckoutSummary ingredients={this.state.ingredients} />
+      </div>
+    );
+  }
+}
