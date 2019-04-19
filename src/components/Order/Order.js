5be20411870ebe33d96d18e4ed0425d9a06d3d5f
afozbek <furkanozbek1995@gmail.com>
2019-04-19 22:11:10 +0300

    fixing bug

--- src/components/Order/Order.js
+++ src/components/Order/Order.js
@@ -4,7 +4,7 @@ import classes from "./Order.css";
 
 const order = props => {
   return (
-    <div class={classes.Order}>
+    <div className={classes.Order}>
       <p>Ingredients: Salad (1)</p>
       <p>
         Price:<strong>$ 5.45</strong> Price (1)
