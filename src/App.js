4505e8817c188f86b1902815087b9d4e27d4150c
afozbek <furkanozbek1995@gmail.com>
2019-04-20 12:04:21 +0300

    removing Link

--- src/App.js
+++ src/App.js
@@ -3,7 +3,7 @@ import React, { Component } from "react";
 import Layout from "./hoc/Layout/Layout";
 import BurgerBuilder from "./containers/BurgerBuilder/BurgerBuilder";
 import Checkout from "./containers/Checkout/Checkout";
-import { Route, Link, Switch } from "react-router-dom";
+import { Route, Switch } from "react-router-dom";
 import Orders from "./containers/Orders/Orders";
 
 class App extends Component {
