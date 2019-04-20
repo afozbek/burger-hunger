7cd25afeec1a5cefb3fe798760e92ade5d5bb2a0
afozbek <furkanozbek1995@gmail.com>
2019-04-20 10:32:23 +0300

    adding Input component

---
+++ src/components/UI/Input/Input.js
@@ -0,0 +1,61 @@
+import React from "react";
+import classes from "./Input.css";
+
+const input = props => {
+  let inputElement = null;
+  switch (props.elementType) {
+    case "input":
+      inputElement = (
+        <input
+          className={classes.InputElement}
+          {...props.elementConfig}
+          value={props.value}
+          onChange={props.changed}
+        />
+      );
+      break;
+    case "textarea":
+      inputElement = (
+        <textarea
+          className={classes.InputElement}
+          value={props.value}
+          onChange={props.changed}
+        />
+      );
+      break;
+    case "select":
+      inputElement = (
+        <select
+          className={classes.InputElement}
+          {...props.elementConfig}
+          value={props.value}
+          onChange={props.changed}
+        >
+          {props.elementConfig.options.map(option => (
+            <option key={option.value} value={option.value}>
+              {option.displayValue}
+            </option>
+          ))}
+        </select>
+      );
+      break;
+    default:
+      inputElement = (
+        <input
+          className={classes.InputElement}
+          {...props.elementConfig}
+          value={props.value}
+          onChange={props.changed}
+        />
+      );
+      break;
+  }
+  return (
+    <div>
+      <label className={classes.Label}>{props.name}</label>
+      {inputElement}
+    </div>
+  );
+};
+
+export default input;
