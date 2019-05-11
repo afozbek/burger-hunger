9c0f80a2caa636da3e1d756312f5d6c5bea2a2b4
afozbek <furkanozbek1995@gmail.com>
2019-05-11 20:53:38 +0300

    actionCreator burgerBuilder

---
+++ src/store/actions/burgerBuilder.js
@@ -0,0 +1,48 @@
+import * as actionTypes from "./actionsTypes";
+import axios from "../../axios-orders";
+
+export const addIngredient = ingName => {
+  return {
+    type: actionTypes.ADD_INGREDIENT,
+    payload: {
+      ingredientName: ingName
+    }
+  };
+};
+
+export const removeIngredient = ingName => {
+  return {
+    type: actionTypes.REMOVE_INGREDIENT,
+    payload: {
+      ingredientName: ingName
+    }
+  };
+};
+
+export const setIngredients = ingredients => {
+  return {
+    type: actionTypes.SET_INGREDIENTS,
+    payload: {
+      ingredients: ingredients
+    }
+  };
+};
+
+export const fetchIngredientsFailed = () => {
+  return {
+    type: actionTypes.FETCH_INGREDIENTS_FAILED
+  };
+};
+
+export const initIngredients = () => {
+  return dispatch => {
+    axios
+      .get("/ingredients.json")
+      .then(ingredients => {
+        dispatch(setIngredients(ingredients.data));
+      })
+      .catch(err => {
+        dispatch(fetchIngredientsFailed());
+      });
+  };
+};
