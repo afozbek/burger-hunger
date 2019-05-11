5797c5321dfc20ee0ee643ee6bf18a93f66b180d
afozbek <furkanozbek1995@gmail.com>
2019-05-11 20:53:51 +0300

    actionCreator order

---
+++ src/store/actions/order.js
@@ -0,0 +1,48 @@
+import * as actionTypes from "./actionsTypes";
+import axios from "../../axios-orders";
+
+export const purchaseBurgerSuccess = (id, orderData) => {
+  return {
+    type: actionTypes.PURCHASE_BURGER_SUCCESS,
+    payload: {
+      orderId: id,
+      orderData: orderData
+    }
+  };
+};
+
+export const purchaseBurgerFail = error => {
+  return {
+    type: actionTypes.PURCHASE_BURGER_FAIL,
+    payload: {
+      error: error
+    }
+  };
+};
+
+export const purchaseBurgerStart = () => {
+  return {
+    type: actionTypes.PURCHASE_BURGER_START
+  };
+};
+
+export const purchaseBurger = orderData => {
+  return dispatch => {
+    dispatch(purchaseBurgerStart());
+    axios
+      .post("/orders.json", orderData)
+      .then(res => {
+        const id = res.data.name;
+        dispatch(purchaseBurgerSuccess(id, orderData));
+      })
+      .catch(err => {
+        dispatch(purchaseBurgerFail(err));
+      });
+  };
+};
+
+export const purchaseInit = () => {
+  return {
+    type: actionTypes.PURCHASE_INIT
+  };
+};
