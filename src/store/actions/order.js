62797f30230a7c2f5479a35b67cbd5e07c52badf
afozbek <furkanozbek1995@gmail.com>
2019-05-12 15:15:11 +0300

    adding auth routes to orders

--- src/store/actions/order.js
+++ src/store/actions/order.js
@@ -26,11 +26,11 @@ export const purchaseBurgerStart = () => {
   };
 };
 
-export const purchaseBurger = orderData => {
+export const purchaseBurger = (orderData, token) => {
   return dispatch => {
     dispatch(purchaseBurgerStart());
     axios
-      .post("/orders.json", orderData)
+      .post("/orders.json?auth=" + token, orderData)
       .then(res => {
         const id = res.data.name;
         dispatch(purchaseBurgerSuccess(id, orderData));
@@ -70,11 +70,11 @@ export const fetchOrdersStart = () => {
   };
 };
 
-export const fetchOrders = () => {
+export const fetchOrders = token => {
   return dispatch => {
     dispatch(fetchOrdersStart());
     axios
-      .get("/orders.json")
+      .get("/orders.json?auth=" + token)
       .then(res => {
         const fetchedOrders = [];
         for (let key in res.data) {
