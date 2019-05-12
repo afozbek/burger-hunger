fe856376781b2690fbbacb213731ef84ed3af105
afozbek <furkanozbek1995@gmail.com>
2019-05-12 15:15:54 +0300

    adding auth functions

--- src/store/reducers/auth.js
+++ src/store/reducers/auth.js
@@ -8,13 +8,40 @@ const initialState = {
   loading: false
 };
 const authStart = (state, action) => {
-  return updateObject();
+  return updateObject(state, { error: null, loading: true });
+};
+
+const authSuccess = (state, action) => {
+  console.log(action.payload.idToken);
+  return updateObject(state, {
+    token: action.payload.idToken,
+    userId: action.payload.userId,
+    error: null,
+    loading: false
+  });
+};
+
+const authFail = (state, action) => {
+  return updateObject(state, {
+    error: action.payload.error,
+    loading: false
+  });
+};
+
+const authLogout = (state, action) => {
+  return updateObject(state, { token: null, userId: null });
 };
 
 const authReducer = (state = initialState, action) => {
   switch (action.type) {
     case actionTypes.AUTH_START:
       return authStart(state, action);
+    case actionTypes.AUTH_SUCCESS:
+      return authSuccess(state, action);
+    case actionTypes.AUTH_FAIL:
+      return authFail(state, action);
+    case actionTypes.AUTH_LOGOUT:
+      return authLogout(state, action);
     default:
       return state;
   }
