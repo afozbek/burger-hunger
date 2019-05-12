c687de642bcc257ed2e8bf504523f5e31dbf51ce
afozbek <furkanozbek1995@gmail.com>
2019-05-12 15:27:20 +0300

    adding conditional rendering

--- src/components/Navigation/NavigationItems/NavigationItems.js
+++ src/components/Navigation/NavigationItems/NavigationItems.js
@@ -8,7 +8,11 @@ const navigationItems = props => (
       Burger Builder
     </NavigationItem>
     <NavigationItem link="/orders">Orders</NavigationItem>
-    <NavigationItem link="/auth">Authenticate</NavigationItem>
+    {!props.isAuth ? (
+      <NavigationItem link="/auth">Authenticate</NavigationItem>
+    ) : (
+      <NavigationItem link="/logout">Logout</NavigationItem>
+    )}
   </ul>
 );
 
