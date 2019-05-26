0b23d3fd03624deb92097b60040b8011f1f17db7
afozbek <furkanozbek1995@gmail.com>
2019-05-26 12:24:07 +0300

    applying utility function

--- src/containers/Auth/Auth.js
+++ src/containers/Auth/Auth.js
@@ -7,6 +7,7 @@ import Button from "../../components/UI/Button/Button";
 import classes from "./Auth.css";
 import * as actions from "../../store/actions/index";
 import Spinner from "../../components/UI/Spinner/Spinner";
+import { updateObject } from "../../shared/utility";
 
 class Auth extends Component {
   state = {
@@ -68,18 +69,16 @@ class Auth extends Component {
   }
 
   inputChangedHandler = (event, controlName) => {
-    const updatedControl = {
-      ...this.state.controls,
-      [controlName]: {
-        ...this.state.controls[controlName],
+    const updatedControl = updateObject(this.state.controls, {
+      [controlName]: updateObject(this.state.controls[controlName], {
         value: event.target.value,
         valid: this.checkValidity(
           event.target.value,
           this.state.controls[controlName].validation
         ),
         touched: true
-      }
-    };
+      })
+    });
     this.setState({ controls: updatedControl });
   };
 
--- src/store/reducers/order.js
+++ src/store/reducers/order.js
@@ -1,5 +1,5 @@
 import * as actionTypes from "../actions/actionsTypes";
-import { updateObject } from "../utility";
+import { updateObject } from "../../shared/utility";
 
 const initialState = {
   orders: [],
@@ -7,9 +7,11 @@ const initialState = {
   purchased: false
 };
 
-const purchaseInit = (state, action) => updateObject(state, { purchased: false })
+const purchaseInit = (state, action) =>
+  updateObject(state, { purchased: false });
 
-const purchaseBurgerStart = (state, action) => updateObject(state, { loading: true })
+const purchaseBurgerStart = (state, action) =>
+  updateObject(state, { loading: true });
 
 const purchaseBurgerSuccess = (state, action) => {
   return updateObject(state, {
@@ -17,45 +19,49 @@ const purchaseBurgerSuccess = (state, action) => {
     purchased: true,
     orders: state.orders.concat(action.payload)
   });
-}
+};
 
-const purchaseBurgerFail = (state, action) => updateObject(state, { loading: false });
+const purchaseBurgerFail = (state, action) =>
+  updateObject(state, { loading: false });
 
-const fetchOrdersStart = (state, action) => updateObject(state, { loading: true });
+const fetchOrdersStart = (state, action) =>
+  updateObject(state, { loading: true });
 
 const fetchOrdersSuccess = (state, action) => {
   return updateObject(state, {
     loading: false,
     orders: action.payload.orders
   });
-}
+};
 
-const fetchOrdersFail = (state, action) => updateObject(state, { loading: false });
+const fetchOrdersFail = (state, action) =>
+  updateObject(state, { loading: false });
 
 const orderReducer = (state = initialState, action) => {
   switch (action.type) {
     case actionTypes.PURCHASE_INIT:
-      return purchaseInit(state, action)
+      return purchaseInit(state, action);
 
     case actionTypes.PURCHASE_BURGER_START:
-      return purchaseBurgerStart(state, action)
+      return purchaseBurgerStart(state, action);
 
     case actionTypes.PURCHASE_BURGER_SUCCESS:
-      return purchaseBurgerSuccess(state, action)
+      return purchaseBurgerSuccess(state, action);
 
     case actionTypes.PURCHASE_BURGER_FAIL:
-      return purchaseBurgerFail(state, action)
+      return purchaseBurgerFail(state, action);
 
     case actionTypes.FETCH_ORDERS_START:
-      return fetchOrdersStart(state, action)
+      return fetchOrdersStart(state, action);
 
     case actionTypes.FETCH_ORDERS_SUCCESS:
-      return fetchOrdersSuccess(state, action)
+      return fetchOrdersSuccess(state, action);
 
     case actionTypes.FETCH_ORDERS_FAIL:
-      return fetchOrdersFail(state, action)
+      return fetchOrdersFail(state, action);
 
-    default: return state;
+    default:
+      return state;
   }
 };
 export default orderReducer;
