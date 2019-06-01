3ef7987286b66792caf5ff149a25d5b3c3bdd2b8
afozbek <furkanozbek1995@gmail.com>
2019-06-01 13:52:38 +0300

    updating auth saga

--- src/store/sagas/auth.js
+++ src/store/sagas/auth.js
@@ -1,5 +1,7 @@
 import { delay } from "redux-saga/effects";
 import { put } from "redux-saga/effects";
+import axios from "axios";
+import { signupUrl, loginUrl } from "../../keys";
 
 import * as actions from "../actions";
 
@@ -13,4 +15,36 @@ export function* logoutSaga(action) {
 export function* checkAuthTimeoutSaga(action) {
   yield delay(action.payload.expirationTime * 1000);
   yield put(actions.logout());
+}
+
+export function* authSaga(action) {
+  yield put(actions.authStart());
+
+  const authData = {
+    email: action.payload.email,
+    password: action.payload.password,
+    returnSecureToken: true
+  };
+  console.log(authData);
+  let url = `${signupUrl}`;
+
+  if (!action.payload.isSignup) {
+    url = `${loginUrl}`;
+  }
+  try {
+    const response = yield axios.post(url, authData);
+
+    const expirationDate = yield new Date(
+      new Date().getTime() + response.data.expiresIn * 1000
+    );
+
+    yield localStorage.setItem("token", response.data.idToken);
+    yield localStorage.setItem("expirationDate", expirationDate);
+    yield localStorage.setItem("userId", response.data.localId);
+
+    yield put(actions.authSuccess(response.data));
+    yield put(actions.checkAuthTimeout(response.data.expiresIn));
+  } catch (err) {
+    yield put(actions.authFail(err.response.data.error));
+  }
 }
