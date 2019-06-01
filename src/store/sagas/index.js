7909cc953fbe91a8380129c60bf2c860b0892c08
afozbek <furkanozbek1995@gmail.com>
2019-06-01 13:52:48 +0300

    updating index saga

--- src/store/sagas/index.js
+++ src/store/sagas/index.js
@@ -1,8 +1,10 @@
 import { takeEvery } from "redux-saga/effects";
 
 import * as actionTypes from "../actions/actionsTypes";
-import { logoutSaga } from "./auth";
+import { logoutSaga, checkAuthTimeoutSaga, authSaga } from "./auth";
 
 export function* watchAuth() {
   yield takeEvery(actionTypes.AUTH_INITIATE_LOGOUT, logoutSaga);
+  yield takeEvery(actionTypes.AUTH_CHECK_TIMEOUT, checkAuthTimeoutSaga);
+  yield takeEvery(actionTypes.AUTH_USER, authSaga);
 }
