a144e4f2490041dc2a3d92665f677d1c825ba269
afozbek <furkanozbek1995@gmail.com>
2019-09-07 15:42:44 +0300

    Updates

---
+++ precache-manifest.3c21418f65b2bdc9d5f97055d4da5a74.js
@@ -0,0 +1,50 @@
+self.__precacheManifest = [
+  {
+    "revision": "a28cedf176149489f77a",
+    "url": "/burger-hunger/static/css/4.b459e35a.chunk.css"
+  },
+  {
+    "revision": "ecfba5f8b55b1206cd40",
+    "url": "/burger-hunger/static/css/main.ffdbc4f5.chunk.css"
+  },
+  {
+    "revision": "c1ae6c0b7ff2a2504e16",
+    "url": "/burger-hunger/static/js/runtime~main.a756a249.js"
+  },
+  {
+    "revision": "616a262de43ddc74caa8",
+    "url": "/burger-hunger/static/js/2.c67a93d2.chunk.js"
+  },
+  {
+    "revision": "b8503d262bffbfb7c67fd6762963e7d1",
+    "url": "/burger-hunger/static/media/burger-logo.b8503d26.png"
+  },
+  {
+    "revision": "a02bb27f08ce51fd4b81",
+    "url": "/burger-hunger/static/js/3.ddb42587.chunk.js"
+  },
+  {
+    "revision": "ecfba5f8b55b1206cd40",
+    "url": "/burger-hunger/static/js/main.371fb861.chunk.js"
+  },
+  {
+    "revision": "a28cedf176149489f77a",
+    "url": "/burger-hunger/static/js/4.9728d91f.chunk.js"
+  },
+  {
+    "revision": "f15efcbe6c1656cbc0e2",
+    "url": "/burger-hunger/static/js/5.40452c55.chunk.js"
+  },
+  {
+    "revision": "f15efcbe6c1656cbc0e2",
+    "url": "/burger-hunger/static/css/5.083aeabd.chunk.css"
+  },
+  {
+    "revision": "a02bb27f08ce51fd4b81",
+    "url": "/burger-hunger/static/css/3.575aee9e.chunk.css"
+  },
+  {
+    "revision": "3f085e909348fdcca3fae6b13c611d4f",
+    "url": "/burger-hunger/index.html"
+  }
+];
